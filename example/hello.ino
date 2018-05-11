a00ad97e921ee9e0ed589821f2b0fc829d9ec6c3
david gauchard <gauchard@laas.fr>
2018-05-12 01:40:48 +0200

    +gcc5.2 (iram is quickly filled, more than with 4.8, like with 7.2) .map example for comparison with simple hello sketch (runs with 5.2) use nproc for make -j rework patches

---
+++ example/hello.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("hello");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
