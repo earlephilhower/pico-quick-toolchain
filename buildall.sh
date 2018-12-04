2ef4ca0d8d6131bc84ab0677d0ee0f185f404b00
Earle F. Philhower, III <earlephilhower@yahoo.com>
2018-12-04 13:11:18 -0800

    Refactor and add other support tools

--- buildall.sh
+++ buildall.sh
@@ -1,11 +1,15 @@
 #/bin/bash
 
 gcc=${1:-4.8}
-echo Multiarch build for: $gcc
+rel=${rel:-2.5.0}
+subrel=${subrel:-2}
+
+echo Multiarch build for: ${gcc} ${rel}-${subrel}
 
 cd /workdir
 
+./build distclean
 for host in linux win64 win32 osx arm64 rpi; do
+	gcc=${gcc} host=${host} rel=${rel} subrel=${subrel} ./build build
 	./build clean
-	gcc=$gcc host=$host ./build build
 done
---
+++ patch_json.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import argparse
+import collections
+import glob
+import json
+import os
+
+parser = argparse.ArgumentParser(description='Patch in a section of the Arduino tools JSON')
+parser.add_argument('--pkgfile', help="Arduino JSON file to update", type=str, required=True)
+parser.add_argument('--tool', help="Name of tool to update", type=str, required=True)
+parser.add_argument('--ver', help="Version of tool to update", type=str, required=True)
+parser.add_argument('--glob', help="Glob to match for tool", type=str, required=True)
+args = parser.parse_args()
+
+# Load the JSON
+with open(args.pkgfile) as f:
+    data = json.load(f, object_pairs_hook=collections.OrderedDict)
+
+for i in range(0, len(data["packages"][0]["platforms"][0]["toolsDependencies"])):
+    if data["packages"][0]["platforms"][0]["toolsDependencies"][i]["name"] == args.tool:
+        data["packages"][0]["platforms"][0]["toolsDependencies"][i]["version"] = args.ver
+
+for i in range(0, len(data["packages"][0]["tools"])):
+    if data["packages"][0]["tools"][i]["name"] == args.tool:
+        print "Patching tool: " + args.tool + " to ver: " + args.ver
+        data["packages"][0]["tools"][i]["version"] = args.ver
+        data["packages"][0]["tools"][i]["systems"] = []
+        for j in glob.glob(args.glob):
+            with open(j) as s:
+                print "Adding: " + j
+                part = json.load(s, object_pairs_hook=collections.OrderedDict)
+                data["packages"][0]["tools"][i]["systems"].append(part)
+
+print "Writing new file " + args.pkgfile
+with open(args.pkgfile, "w") as f:
+    f.write(json.dumps(data, indent=3, separators=(',',': ')))
