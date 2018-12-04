74a4bee8cf478d9138cbf11915b66a120a3f6dd3
Earle F. Philhower, III <earlephilhower@yahoo.com>
2018-12-03 21:57:25 -0800

    Clean before building all

--- buildall.sh
+++ buildall.sh
@@ -6,6 +6,6 @@ echo Multiarch build for: $gcc
 cd /workdir
 
 for host in linux win64 win32 osx arm64 rpi; do
-	gcc=$gcc host=$host ./build build
 	./build clean
+	gcc=$gcc host=$host ./build build
 done
