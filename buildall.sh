0711e06e5bbe3151af341eb469a7f76bd9a625e4
Earle F. Philhower, III <earlephilhower@yahoo.com>
2018-12-04 20:56:59 -0800

    Add auto-upload, fix spiffs to 0.2.0

--- buildall.sh
+++ buildall.sh
@@ -13,3 +13,17 @@ for host in linux win64 win32 osx arm64 rpi; do
 	gcc=${gcc} host=${host} rel=${rel} subrel=${subrel} ./build build
 	./build clean
 done
+
+gcc=${gcc} host=${host} rel=${rel} subrel=${subrel} ./build install
+
+pass=$(cat .ghpass)
+rm -rf ./venv
+mkdir ./venv
+virtualenv --no-site-packages venv
+pushd ./venv
+source bin/activate
+pip install -q pygithub
+python ../upload_release.py --user earlephilhower --pw "${pass}" --tag ${rel}-${subrel} --msg 'See https://github.com/esp8266/Arduino for more info'  --name "ESP8266 Quick Toolchain for ${rel}-${subrel}" ../*.tar.gz ../*.zip
+deactivate
+popd
+rm -rf ./venv
